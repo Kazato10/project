bad184818a94210607a197accbc42c4a00eea537
Kazato10 <89738134+Kazato10@users.noreply.github.com>
2023-07-04 23:34:37 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,46 +1,79 @@
 from tkinter import *
 import os
 
-class Application(Frame):
-    def __init__(self,master = None):
-        super().__init__(master)
-        self.master = master
-        self.pack()
+class main_page():
+    def __init__(self,window):
+        self.window = window
+        #屏幕位置
+        sw = self.window.winfo_screenwidth()
+        sh = self.window.winfo_screenheight()
+        #脚本屏幕大小
+        ww = 400
+        wh = 200
+        #脚本x,y
+        x = (sw-ww)/2
+        y = (sh-wh)/2
+        self.window.title('文件脚本')
+        self.window.geometry("%dx%d+%d+%d" % (ww,wh,x,y))
+        self.createPage()
+
+    def createPage(self):
+        self.btn01 = Button(self.window,text="文件删除",command=self.change_one)
+        self.btn01.pack()
+
+    def change_one(self):
+        self.btn01.destroy()
+        Applicationone(screen)
+        
+class Applicationone():
+    def __init__(self,window):
+        self.window = window
+        #屏幕位置
+        sw = self.window.winfo_screenwidth()
+        sh = self.window.winfo_screenheight()
+        #脚本屏幕大小
+        ww = 400
+        wh = 200
+        #脚本x,y
+        x = (sw-ww)/2
+        y = (sh-wh)/2
+        self.window.title('文件脚本')
+        self.window.geometry("%dx%d+%d+%d" % (ww,wh,x,y))
         self.createWidget()
 
     def createWidget(self):
         # 创建说明组件
-        self.label01 = Label(self, text="请根据自身需求在下方输入输入文件路径")
+        self.label01 = Label(self.window, text="请根据自身需求在下方输入输入文件路径")
         self.label01.pack()
 
         # 创建路径输入框
         # StringVar 变量绑定到指定的组件
         v1 = StringVar()
-        self.Entry01 = Entry(self,textvariable=v1)
+        self.Entry01 = Entry(self.window,textvariable=v1)
         self.Entry01.pack()
 
         # 创建说明组件
-        self.label02 = Label(self, text="请根据自身需求在下方输入文件词")
+        self.label02 = Label(self.window, text="请根据自身需求在下方输入文件词")
         self.label02.pack()
         # 创建时间输入框
         v2 = StringVar()
-        self.Entry02 = Entry(self,textvariable=v2)
+        self.Entry02 = Entry(self.window,textvariable=v2)
         self.Entry02.pack()
 
         # 创建说明组件
-        self.label03 = Label(self, text="请根据自身需求在下方输入文件后缀")
+        self.label03 = Label(self.window, text="请根据自身需求在下方输入文件后缀")
         self.label03.pack()
         # 创建后缀输入框
         v3 = StringVar()
-        self.Entry03 = Entry(self,textvariable=v3)
+        self.Entry03 = Entry(self.window,textvariable=v3)
         self.Entry03.pack()
 
         # 创建删除组件
-        self.btn02 = Button(self,text='确认删除',command=self.delete)
+        self.btn02 = Button(self.window,text='确认删除',command=self.delete)
         self.btn02.pack()
 
         # 创建退出组件
-        self.btnQuit = Button(self,text='退出',command=screen.destroy)
+        self.btnQuit = Button(self.window,text='退出',command=screen.destroy)
         self.btnQuit.pack()
 
     def delete(self):
@@ -58,23 +91,11 @@ class Application(Frame):
                 new_path = path + a + file
                 os.remove(new_path)
                 
-
 screen = Tk()
-#屏幕位置
-sw = screen.winfo_screenwidth()
-sh = screen.winfo_screenheight()
-#脚本屏幕大小
-ww = 400
-wh = 200
-#脚本x,y
-x = (sw-ww)/2
-y = (sh-wh)/2
-
-screen.title('文件脚本')
-screen.geometry("%dx%d+%d+%d" % (ww,wh,x,y))
-app = Application(master=screen)
+page_one = main_page(screen)
 
 screen.mainloop()
+
 
 # F:\xunlei\合集\2
 # 工作表1
